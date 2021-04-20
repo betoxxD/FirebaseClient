2b4df0b97a40e61204448854f6060c4c4d84e3ae
Alberto <betoxxdiosnava@gmail.com>
2021-04-20 18:55:20 -0500

    Initial commit

---
+++ app/src/main/java/com/betoxx/firebaseclient/MainActivity.java
@@ -0,0 +1,40 @@
+package com.betoxx.firebaseclient;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.messaging.FirebaseMessaging;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "HOLI";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FirebaseMessaging.getInstance().getToken()
+                .addOnCompleteListener(new OnCompleteListener<String>() {
+                    @Override
+                    public void onComplete(@NonNull Task<String> task) {
+                        if(!task.isSuccessful()) {
+                            Log.w(TAG, "Fetching FCM registration token failed", task.getException());
+                            return;
+                        }
+
+                        String token = task.getResult();
+
+                        String msg = token;
+                        Log.d(TAG,msg);
+                        Toast.makeText(MainActivity.this,msg, Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+}
